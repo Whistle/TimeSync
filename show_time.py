93f0dc9c30c023a086059b6ba55c9312ef77f8ad
Dominic Prass <dominic.prass@googlemail.com>
2019-12-06 08:58:39 +0000

    Add payload construction

--- show_time.py
+++ show_time.py
@@ -7,11 +7,24 @@ from time import sleep
 
 while True:
     dt = datetime.utcnow()
-    print(dt)
     delta = timedelta(seconds=1)
-    print(dt + delta)
     ms = dt.microsecond/1000.0
-    print(ms)
     s = ms/1000.0
-    print(s)
     sleep(1.0 - s)
+
+    # setup payload
+    msg = "{:04d}{:02d}{:02d}{:02d}{:02d}{:02d}".format(dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second)
+    payload = bytearray()
+    # Add header
+    payload.append(0x55)
+    payload.extend(map(ord, msg))
+    # build checksum
+    checksum = 0
+    for b in payload:
+        checksum = checksum ^ b
+    payload.append(checksum)
+    # mark message end
+    payload.append(0xAA)
+    with open('serial_dev', 'wb') as f:
+        f.write(payload)
+    break
